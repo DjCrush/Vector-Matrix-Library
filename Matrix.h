0aba788019e5368c483e9237f7ae31800ab902bd
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-07-09 15:37:54 +0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -2,27 +2,31 @@
 #define _MATRIX_H
 #include <initializer_list>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 class Matrix
 {
+	double a[3][3];
 public:
-	Matrix();
+	Matrix(); // default constructor
 	Matrix(std::initializer_list<double> a, std::initializer_list<double> b, std::initializer_list<double> c);
 	Matrix(double a[][3]);
-	Matrix operator+(const Matrix& rhs);
 	Matrix& operator+=(const Matrix& rhs);
 	Matrix& operator-=(const Matrix& rhs);
-	Matrix operator*(const double& rhs);
 	Matrix operator=(const Matrix& rhs);
-	Matrix operator*(const Matrix& rhs); // Matrix multiplication 
-	Matrix operator-(const Matrix& rhs);
-private:
 	friend ostream& operator<<(ostream& out, const Matrix& m);
-	double a[3][3];
+	friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
+	friend Matrix operator-(const Matrix& lhs, const Matrix& rhs);
+	friend Matrix operator*(const Matrix& lhs, const Matrix& rhs);
+	friend Matrix operator*(const Matrix& lhs, const double& rhs);
 };
 
 ostream& operator<<(ostream& out, const Matrix& m);
+Matrix operator+(const Matrix& lhs, const Matrix& rhs);
+Matrix operator-(const Matrix& lhs, const Matrix& rhs);
+Matrix operator*(const Matrix& lhs, const Matrix& rhs);
+Matrix operator*(const Matrix& lhs, const double& rhs);
 
 #endif // !_MATRIX_H
